af1adecf31fb4994f30c6026b09ef3b2dd610b24
MdKawsarAK <kawsarhossain830@gmail.com>
2025-12-23 14:36:39 +0600

    bootstrap template

---
+++ reactapp4/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route ,Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <Router>
+      <div>
+       {/* Navbar will  added */}
+       <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+
+
+export default App;
